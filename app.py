d1a004e751897c88063caffbf8153de1e6486865
jordanterrell <jordan.terrell@probonoindiana.org>
2025-07-07 13:00:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -75,12 +75,10 @@ def save_event_and_participants():
         participant_df.to_excel(writer, sheet_name='Participant Submissions', index=False)
 
 
-import os
-
 if __name__ == '__main__':
-    from app import app  # if your app is defined in a separate file
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
+
 
 
     load_excel()
